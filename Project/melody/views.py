ab537cfe09c78b2fb9535adddbdfc837b1532826
Rese574 <therese.bolabola@cit.edu>
2020-09-28 16:04:41 +0800

    Update and Delete of CRUD

--- Project/melody/views.py
+++ Project/melody/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic import View, TemplateView
+from django.views.generic import View
 from .forms import *
 from .models import *
 
@@ -18,6 +19,25 @@ class MelodyProductDashboardView(View):
 			'songs' : songs,
 		}
 		return render(request, 'melody/productDashboard.html', context)
+	def post(self, request):
+		if request.method == 'POST':
+			if 'btnUpdate' in request.POST:
+				sid = request.POST.get("song-id")
+				title = request.POST.get("song-title")
+				date = request.POST.get("song-release")
+				print(date)
+				artist = request.POST.get("song-artists")
+				genre = request.POST.get("song-genre")
+				writers = request.POST.get("song-writer")
+				producer = request.POST.get("song-producer")
+				update_song = Song.objects.filter(id = sid).update(songtitle = title, genre = genre, artist = artist, dateRelease = date, producer = producer, songwriter = writers)
+				print(update_song)
+			elif 'btnDelete' in request.POST:
+				sid = request.POST.get("song-id")
+				song = Song.objects.filter(id = sid).delete()
+				print('record deleted')
+			return redirect('melody:melody_productDashboard_view')
+
 
 class MelodyCustomerDashboardView(View):
 	def get(self, request):
@@ -26,6 +46,24 @@ class MelodyCustomerDashboardView(View):
 			'customers' : customers,
 		}
 		return render(request, 'melody/customerDashboard.html', context)
+	def post(self, request):
+		if request.method == 'POST':
+			if 'btnUpdate' in request.POST:
+				cid = request.POST.get("customer-id")
+				fname = request.POST.get("customer-fname")
+				lname = request.POST.get("customer-lname")
+				date = request.POST.get("customer-bday")
+				print(date)
+				add = request.POST.get("customer-add")
+				email = request.POST.get("customer-email")
+				contact = request.POST.get("customer-contact")
+				update_customer = Customer.objects.filter(id = cid).update(firstname = fname, lastname = lname, birthday = date, address = add, email = email, contact = contact)
+				print(update_customer)
+			elif 'btnDelete' in request.POST:
+				cid = request.POST.get("customer-id")
+				customer = Customer.objects.filter(id = cid).delete()
+				print('record deleted')
+			return redirect('melody:melody_customerDashboard_view')
 
 class MelodyCustomerRegistrationView(View):
 	def get(self, request):
@@ -42,7 +80,7 @@ class MelodyCustomerRegistrationView(View):
 			contact = request.POST.get("contact")
 			form = Customer(firstname = fname, lastname = lname, birthday = bday, address = add, email = email, password = password, contact = contact)
 			form.save()
-			return HttpResponse("Customer added")
+			return redirect('melody:melody_customerDashboard_view')
 		else:
 			print(form.errors)
 			return HttpResponse("Form is invalid")
@@ -65,7 +103,7 @@ class MelodySongRegistrationView(View):
 			songwriter = request.POST.get("songwriter")
 			form = Song(songtitle = title, genre = genre, artist = artist, dateRelease = date, producer = producer, songwriter = songwriter)
 			form.save()
-			return HttpResponse("Song has been added!!!")
+			return redirect('melody:melody_productDashboard_view')
 		else:
 			print(form.errors)
 			return HttpResponse("Form is invalid")		
